072a68cc9453940bf2c6b7f432a709dd14fc8353
1M-GA5 <athrvgajbhiyexxi@gmail.com>
2023-04-12 15:05:09 +0530

    Editing and Deleting Expenses

--- src/components/context/ExpenseContext.jsx
+++ src/components/context/ExpenseContext.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const expenseContext = createContext()
 
@@ -8,10 +10,17 @@ const ExpenseProvider = ({children}) => {
     useEffect(() => {
         getExpenses()
     }, []);
+const ExpenseProvider = ({ children }) => {
+  const [expenses, setExpenses] = useState([]);
+  useEffect(() => {
+  getExpenses()
+  }, []);
 
     async function getExpenses() {
+      async function getExpenses() {
         const res = await axios.get(
             "https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses.json"
+          "https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses.json"
         );
 
         setExpenses([])
@@ -19,6 +28,9 @@ const ExpenseProvider = ({children}) => {
                 setExpenses((prev) => [...prev, res.data[key]])
             }
     }
+          for (let key in res.data) {
+            setExpenses((prev) => [...prev, { id : key,...res.data[key] }])
+        }
 
     async function postExpense(item) {
         const res = await axios.post(
@@ -28,11 +40,42 @@ const ExpenseProvider = ({children}) => {
         getExpenses()
     }
 
+  async function deleteExpense(id) {
+    await axios.delete(
+      `https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses/${id}.json`
+    );
+    getExpenses()
+  }
+
+    async function editExpense(id,item) {
+      const res = await axios.put(
+        `https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses/${id}.json`,
+        item
+      );
+      console.log(res)
+      getExpenses();
+  }
+
+     async function postExpense(item) {
+      const res =  await axios.post(
+         "https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses.json"
+       ,item);
+      // console.log(res)
+        getExpenses()
+     }
+
     return (
         <expenseContext.Provider value={{ expenses, postExpense }}>
             {children}
         </expenseContext.Provider>
     )
+  return (
+    <expenseContext.Provider
+      value={{ deleteExpense, expenses, postExpense, editExpense }}
+    >
+      {children}
+    </expenseContext.Provider>
+  );
 };
 
 export default ExpenseProvider;
